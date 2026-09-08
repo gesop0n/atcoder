bc4e4075f86ae0ebc4a367869f52994217250eb6
gesop0n <ishikuro6.2@gmail.com>
2026-09-09 01:03:55 +0900

    refactor: minimize C++ solution template

--- 2026/09/09/abc300/f/main.cpp
+++ 2026/09/09/abc300/f/main.cpp
@@ -1,12 +1,3 @@
-#include <bits/stdc++.h>
-#include <atcoder/all>
-
-using namespace std;
-using namespace atcoder;
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     return 0;
 }
