78b2c9506c8233203d772cd79d62497d65a03c1b
gesop0n <ishikuro6.2@gmail.com>
2026-09-09 01:43:33 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -62,7 +62,12 @@
 
         # 普段問題を解くための環境。atcli 自体はリリースビルド済みのものを使う。
         devShells.default = pkgs.mkShell {
-          packages = cppTools ++ [ atcli ];
+          packages = cppTools ++ [
+            atcli
+            # NOTE: direnv で rust-analyzer をインストールしている関係で
+            # default に rust を追加しないと editor で lsp が動かない
+            rust
+          ];
         };
 
         # atcli の開発環境。E2E テストもできるよう C++ toolchain を含める。
