d8dc0e43bb688e03987037135b49d39881798acb
gesop0n <ishikuro6.2@gmail.com>
2026-09-09 10:12:50 +0900

    update: ABC300

--- 2026/09/09/abc300/a/main.cpp
+++ 2026/09/09/abc300/a/main.cpp
@@ -1,18 +1,5 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
-int main() {
-    int N, A, B;
-    cin >> N >> A >> B;
 
-    int ctmp;
-    for (int i = 0; i < N; i++) {
-        cin >> ctmp;
-        if (A + B == ctmp) {
-            cout << i + 1 << "\n";
-            return 0;
-        }
-    }
-
-    return -1;
-}
+int main() { return 0; }
