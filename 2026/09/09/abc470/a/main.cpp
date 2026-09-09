533094156231ce07d89d66cf0df20376f9e023ef
gesop0n <ishikuro6.2@gmail.com>
2026-09-09 09:38:38 +0900

    feat: ABC470 A

---
+++ 2026/09/09/abc470/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        if (i + 1 % 3 == 0) {
+            cout << "Fizz" << "\n";
+        } else {
+            cout << i + 1 << "\n";
+        }
+    }
+    return 0;
+}
