91a5781012da03f50b8c034b4f865e834b5a6e1d
gesop0n <ishikuro6.2@gmail.com>
2026-09-14 00:45:11 +0900

    Add 鉄則 A05

---
+++ attempts/2026/09/13/tessoku-book/a05/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    int ans = 0;
+    for (int x = 1; x <= N; ++x)
+        for (int y = 1; y <= N; ++y) {
+            int z = K - x - y;
+            if (z >= 1 && z <= N) ++ans;
+        }
+
+    cout << ans << '\n';
+}
