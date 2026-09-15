81419d07e37175a695db0dae12660468ed52ebd7
gesop0n <ishikuro6.2@gmail.com>
2026-09-16 01:10:26 +0900

    Solve ABC462 B: Gift

---
+++ attempts/2026/09/16/abc462/b/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<vector<int>> ans(N);
+    for (int i = 0; i < N; ++i) {
+        int K;
+        cin >> K;
+        for (int j = 0; j < K; ++j) {
+            int A;
+            cin >> A;
+            ans[A - 1].push_back(i + 1);
+        }
+    }
+
+    for (int i = 0; i < N; ++i) {
+        cout << ans[i].size();
+
+        for (int person : ans[i]) cout << ' ' << person;
+
+        cout << '\n';
+    }
+}
