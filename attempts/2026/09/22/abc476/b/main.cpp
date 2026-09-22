2751d5e534c73a547f15b27c66dbd0610181ded3
gesop0n <ishikuro6.2@gmail.com>
2026-09-22 21:31:01 +0900

    Solve ABC476 B: Wild Card

---
+++ attempts/2026/09/22/abc476/b/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int N;
+    string S, T;
+    cin >> N;
+    cin >> S >> T;
+    for (int i = 0; i < N; ++i) {
+        if (T[i] != '*' && S[i] != T[i]) {
+            cout << "No\n";
+            return 0;
+        }
+    }
+
+    cout << "Yes\n";
+}
