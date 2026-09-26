e91943e1ca2b8e213368859b8f2cc4ec69ecae13
gesop0n <ishikuro6.2@gmail.com>
2026-09-26 22:31:56 +0900

    Solve ABC477 A: Traffic Light

---
+++ attempts/2026/09/26/abc477/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    char c;
+    cin >> c;
+
+    if (c == 'B')
+        cout << 'Y';
+    else if (c == 'Y')
+        cout << 'R';
+    else if (c == 'R')
+        cout << 'B';
+
+    cout << '\n';
+}
